264bf892ec55ab09673176ea0c16322f8a7933f0
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-30 18:50:30 +0000

    add webpage and story board

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * this function find movie type based on user age
+ */
+function findDiscount() {
+  // input
+  const dayOfTheWeek = document.getElementById("day-of-the-week").value
+  const userAge = parseFloat(document.getElementById("user-age").value)
+
+  // process
+  if (( dayOfTheWeek == "Tuesday" || dayOfTheWeek == "Thursday" ) || (userAge > 12 && userAge <21)) {
+    document.getElementById("result").innerHTML = "You can go into the museum with a discount"
+  } else {
+    document.getElementById("result").innerHTML = "You can go into the museum with a normal price"
+  }
+} 
